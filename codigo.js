2f753b678d28d38c0c6cbad30511496cf9ec6460
tito <diegolverahermoso@gmail>
2021-11-10 11:13:37 -0300

    se agrega el proyecto

---
+++ codigo.js
@@ -0,0 +1,22 @@
+nombre = prompt("Ingrese su Nombre: ");
+apellido = prompt("Ingrese su Apellido: ");
+edad = prompt("Ingrese su edad: ");
+hobbie = prompt("Ingrese su hobbie (pasatiempo): ");
+codigoEditor = prompt("Cual es su editor de código preferido?: ");
+sistemaOperativo = prompt("Que sistema operativo usa en su computadora?: ");
+console.log("DATOS INGRESADOS")
+console.log("Tu nombre es: " + nombre + " " + apellido);
+console.log("Tu edad es: " + edad + " años.");
+console.log("Tu hobbie es: " + hobbie);
+console.log("El editor de cógido que te gusta es: " + codigoEditor);
+console.log("El sistema operativo que usas en tu computadora es: " + sistemaOperativo);
+
+
+document.write("DATOS INGRESADOS")
+document.write("<br> Tu nombre es: " + nombre + " " + apellido);
+document.write("<br> Tu edad es: " + edad + " años.");
+document.write("<br> Tu hobbie es: " + hobbie);
+document.write("<br> El editor de cógido que te gusta es: " + codigoEditor);
+document.write("<br> El sistema operativo que usas en tu computadora es: " + sistemaOperativo);
+
+
